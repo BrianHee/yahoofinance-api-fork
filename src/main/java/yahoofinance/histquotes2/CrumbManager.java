747d5143483665587485c0f3040cc1b88fd3d9f8
Brian Kim <gittybrian@gmail.com>
2024-01-24 14:57:44 -0500

    initial fork: added crumb param to base quote query

--- src/main/java/yahoofinance/histquotes2/CrumbManager.java
+++ src/main/java/yahoofinance/histquotes2/CrumbManager.java
@@ -162,6 +162,7 @@ public class CrumbManager {
 
         Map<String, String> requestProperties = new HashMap<String, String>();
         requestProperties.put("Cookie", cookie);
+        requestProperties.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36");
 
         URLConnection crumbConnection = redirectableCrumbRequest.openConnection(requestProperties);
         InputStreamReader is = new InputStreamReader(crumbConnection.getInputStream());
